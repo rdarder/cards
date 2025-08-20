398c8417b73ff98fe5b3f31882157545a34039a6
Rafael Darder <darder@gmail.com>
2025-08-20 12:03:13 -0300

    move generated html to docs for gh-pages

--- make_html.sh
+++ make_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pandoc --number-sections --toc=true --toc-depth=2\
     --metadata title="Code & Sorcery: A Lua Card Game Quest" \
-    -s -c style.css README.md -o index.html
+    -s -c style.css README.md -o docs/index.html
--- publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# just push main to both main and gh-pages.
-git push origin main:main main:gh-pages
-
